79ff70f70415d39c4c2dabec5f97a134d7a04d30
sagarika581997 <95922056+sagarika581997@users.noreply.github.com>
2021-12-24 13:01:41 +0530

    Add files via upload

---
+++ corejavaassignment9/Trader.java
@@ -0,0 +1,24 @@
+package corejavaAssignment9;
+
+public class Trader {
+	String name;
+	String city;
+
+	public Trader(String name,String city) {
+	this.name=name;
+	this.city=city;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public String toString() {
+		return "Trader[name="+name+",city="+city+"]";
+	}
+
+}
